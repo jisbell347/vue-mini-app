13d18fa9dab9b7a2b6e53a8c0ee3af8732623f36
Joseph Isbell <shadowwolf347@comcast.net>
2018-12-24 09:25:09 -0700

    Added animations.

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,19 @@
                 <h1 class="text-center">The Super Quiz</h1>
             </div>
         </div>
-        <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
-                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component> 
+                </transition>
             </div>
-        </div>
+        </div>        
     </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
     import Answer from './components/Answer.vue';
+    import Question from './components/Question.vue';
 
     export default {
         data() {
@@ -26,20 +25,52 @@
                 mode: 'app-question'
             }
         },
-        methods: {
-          answered(isCorrect) {
-              if (isCorrect) {
-                  this.mode = 'app-answer';
-              } else {
-                  this.mode = 'app-question';
-                  alert('Wrong, try again!');
-              }
-          }
-        },
         components: {
-            appQuestion: Question,
-            appAnswer: Answer
+            appAnswer: Answer,
+            appQuestion: Question
+        }, 
+        methods: {
+            answered(isCorrect) {
+                if(isCorrect) {
+                    this.mode = 'app-answer';
+                } else {
+                    this.mode = 'app-question';
+                    alert("Wrong, try again!");
+                }
+            }
         }
     }
 </script>
+
+<style>
+    .flip-enter{
+        /* transform: rotateY(0deg); */
+
+    }
+    .flip-enter-active {
+        animation: flip-in 0.5s ease-out forwards;
+    }
+    .flip-leave {
+        /* transform: rotateY(0deg); */
+    }
+    .flip-leave-active {
+        animation: flip-out .5s ease-out forwards;
+    }
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
+</style>
 
